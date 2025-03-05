630512f9aac67bd008cda159809f544f1abff2df
Youssef Ramy <youssef.rfarid@gmail.com>
2025-03-05 15:52:02 +0200

    testing multiple functions

---
+++ functions/mySecondFunction/index.mjs
@@ -0,0 +1,10 @@
+// functions/myNewFunction/index.mjs
+export const handler = async (event, context) => {
+  // Your function logic here
+  return {
+    statusCode: 200,
+    body: {
+      message: "Hello from mySecondFunction!",
+    },
+  };
+};
