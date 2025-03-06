3d0b8908f776dcd9063850d22065079414993b17
Youssef Ramy <youssef.rfarid@gmail.com>
2025-03-06 12:44:53 +0200

    removed the auto run

--- terraform/auto_generated_functions.tf
+++ terraform/auto_generated_functions.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "myNewFunction" {
   function_name = "${var.feature_name}-myNewFunction"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "index.handler"
-  runtime       = "nodejs22.x"
+  runtime       = "nodejs18.x"
   filename      = "${path.module}/build/myNewFunction.zip"
 }
 output "myNewFunction_name" {
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "mySecondFunction" {
   function_name = "${var.feature_name}-mySecondFunction"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "index.handler"
-  runtime       = "nodejs22.x"
+  runtime       = "nodejs18.x"
   filename      = "${path.module}/build/mySecondFunction.zip"
 }
 output "mySecondFunction_name" {
